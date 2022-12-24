fcf79066acb527eb281ef17dd6a0edb7f3784ddd
Osvaldo <ojimenez@pacific.edu>
2022-12-24 07:41:10 -0800

    Trying to redo pages again

--- src/i18n.js
+++ src/i18n.js
@@ -2,16 +2,12 @@ import i18n from 'i18next';
 
 import en from './locales/en';
 import fi from './locales/fi';
-import zh from './locales/zh';
 import es from './locales/es';
-import fr from './locales/fr';
 
 const resources = {
   en,
   fi,
-  zh,
   es,
-  fr,
 };
 
 i18n.init({
